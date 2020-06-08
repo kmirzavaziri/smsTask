db44cdb3c76c2ae633185220eea8de901c4f7e6b
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2020-06-09 02:45:54 +0430

    design code structure, add routes

---
+++ Sms.php
@@ -0,0 +1,31 @@
+<?php
+
+class Sms{
+    // send sms by calling send sms APIs
+    public function send(){
+        echo "send";
+        // TODO
+    }
+
+    public function report(){
+        echo "report";
+        // TODO
+    }
+
+    public function search($phone){
+        echo "search: $phone";
+        // TODO
+    }
+
+    public function getCount(){
+        // TODO
+    }
+
+    public function getApiInformation(){
+        // TODO
+    }
+
+    public function getTopTen(){
+        // TODO
+    }
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+# index.php
+# this file only connects defined routes to methods of class Sms in Sms.php
+require_once './vendor/autoload.php';
+require_once 'Sms.php';
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+try{
+    $fileLocator = new FileLocator(array(__DIR__));
+ 
+    $requestContext = new RequestContext();
+    $requestContext->fromRequest(Request::createFromGlobals());
+ 
+    // load all routes from routes.yaml
+    $router = new Router(
+        new YamlFileLoader($fileLocator),
+        'routes.yaml',
+        ['cache_dir' => __DIR__.'/cache'],
+        $requestContext
+    );
+ 
+    // find the current route
+    $parameters = $router->match($requestContext->getPathInfo());
+    
+    // call the related function
+    $passParameters = [];
+    foreach($parameters as $key => $value)
+        if($key[0] != "_")
+            $passParameters[$key] = $value;
+    call_user_func_array($parameters['_controller'], $passParameters);
+
+}
+catch (ResourceNotFoundException $e){
+    echo $e->getMessage();
+}
